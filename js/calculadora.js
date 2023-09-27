42d28f1efd2cc1f2a03f0cb568f56c6ea2048439
Victória Novaes <105504801+ViNovaes@users.noreply.github.com>
2023-09-27 17:43:59 -0300

    Conectado

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,74 +1,28 @@
 //Tentaiva de conectar com o html css feito
 
 
-/* function calcularGorjeta() {
-    let divContaPorcen
+function calcularGorjeta() {
+
     let porcentagem = document.getElementById("porcentagem").value;
     let valorConta = document.getElementById("valorConta").value;
-    let valorGorjeta = document.getElementById("valorGorjeta").value;
-    let valorTotal = document.getElementById("valorTotal").value;
 
-    if (porcentagem === 0.10) {
-        valorGorjeta = (parseInt(valorConta) * parseInt(porcentagem))
-        divContaPorcen = valorGorjeta /100
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    } else if (porcentagem === 0.8) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem) /100)
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    } else if (porcentagem === 0.5) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem) /100)
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    } else if (porcentagem === 0.2) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem))
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    }else {
-        alert = "Você digitou algo errado. Tente novamente!"
-        console.log (`Você digitou algo errado. Tente novamente!`)
+    if (porcentagem == 10) {
+        calculoGorjeta = ((valorConta * 10) / 100)
+        valorTotal = (parseInt(calculoGorjeta) + parseInt(valorConta))
+    } else if (porcentagem == 8) {
+        calculoGorjeta = ((valorConta * 8) / 100)
+        valorTotal = (parseInt(calculoGorjeta) + parseInt(valorConta))
+    } else if (porcentagem == 5) {
+        calculoGorjeta = ((valorConta * 5) / 100)
+        valorTotal = (parseInt(calculoGorjeta) + parseInt(valorConta))
+    } else if (porcentagem == 2) {
+        calculoGorjeta = ((valorConta * 2) / 100)
+        valorTotal = (parseInt(calculoGorjeta) + parseInt(valorConta))
     }
-}
-
-document.getElementById("valorGorjeta").innerHTML = `Sua conta deu R$: ${valorGorjeta}.`
-document.getElementById("valorTotal").innerHTML = `Sua conta deu R$: ${valorTotal}.` */
-
-
-
-//Tentaiva de fazer funcionar com o console.log ANTES DE TENTAR CONECTAR COM O HTML CSS
-
-function calcularGorjeta2() {
-    let porcentagem, valorConta, valorGorjeta, valorTotal
-
-    porcentagem = 0.10
-    valorConta = 100
-
-    if (porcentagem === 0.10) {
-        valorGorjeta = (valorConta * 0.10) /100
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-
-        console.log (`Você vai pagar R$: ${valorGorjeta} de gorjeta.`)
-        console.log (`Sua conta deu no total R$: ${valorTotal}.`) 
-    } else if (porcentagem === 0.8) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem) /100)
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    } else if (porcentagem === 0.5) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem) /100)
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    } else if (porcentagem === 0.2) {
-        valorConta = (parseInt(valorConta) * parseInt(porcentagem))
-        valorTotal = (parseInt(valorGorjeta) + parseInt(valorConta))
-    }else {
-        alert = "Você digitou algo errado. Tente novamente!"
-        console.log (`Você digitou algo errado. Tente novamente!`)
+    else {
+        document.getElementById("total").innerHTML = "Você digitou algo errado. Tente novamente!"
     }
-}
 
-/* console.log (`Você vai pagar R$: ${valorGorjeta} de gorjeta.`)
-console.log (`Sua conta deu no total R$: ${valorTotal}.`) */
-
-
-function somaNumero(){
-    let num1, num2, resp=0
-    num1 = 10
-    num2 = 20
-    resp = num1+num2
-    console.log(`O resultado da soma é ${resp}`)
+    document.getElementById("gorjeta").innerHTML = `R$ ${calculoGorjeta} reais.`
+    document.getElementById("total").innerHTML = `R$ ${valorTotal} reais.`
 }
